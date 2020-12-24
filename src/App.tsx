b7c691d02120852831345522f4a62e2d6ac0e75e
fyui001 <yun.cod@gmail.com>
2020-12-25 01:16:18 +0900

    トップページはこんな感じで

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,13 @@ import React from 'react'
 
 import './App.scss'
 
-import logo from './logo.svg'
-
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <h1>Hello World</h1>
+    <div className="top-page">
+      <div className="title">
+        <h1 className="main-title">New World</h1>
+        <div className="description">服薬指導室</div>
+      </div>
     </div>
   )
 }
