397942969867531d6c0f858449a2d8cea8bb24a9
fyui001 <yun.cod@gmail.com>
2022-11-20 13:49:55 +0900

    パスワード認証用の画面作成

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import MyPage from './pages/MyPage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import { createRoot } from 'react-dom/client'
+import DefinitiveRegister from './pages/DefinitiveRegister'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
@@ -18,6 +19,7 @@ root.render(
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/my-page" element={<MyPage />} />
+        <Route path="/definitive_register" element={<DefinitiveRegister />} />
       </Routes>
     </BrowserRouter>
   </RecoilRoot>
